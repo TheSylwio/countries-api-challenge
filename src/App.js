fba5005816532522af9a1aecf9348f64b4966595
thesylwio <thesylwio@gmail.com>
2020-09-01 22:58:34 +0200

    ✨ Fetching API data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from "./components/Card";
 import Filter from "./components/Filter";
 import Navbar from "./components/Navbar";
@@ -6,13 +6,23 @@ import SearchBar from "./components/SearchBar";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(result => result.json())
+      .then(result => setCountries(() => result))
+  }, []);
+
   return (
     <div className="app">
       <Navbar/>
       <div className="app__content">
         <SearchBar/>
         <Filter/>
-        <Card/>
+        {countries.map(({flag, name, population, region, capital}) => (
+          <Card key={name} flag={flag} name={name} population={population} region={region} capital={capital}/>
+        ))}
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Card.css';
 
-function Card() {
+function Card(props) {
   return (
     <div className='card'>
-      <img className='card__flag' src="https://restcountries.eu/data/deu.svg" alt="Flag"/>
+      <img className='card__flag' src={props.flag} alt={props.name + ' flag'}/>
       <div className="card__content">
-        <header className='content__header'>Germany</header>
+        <header className='content__header'>{props.name}</header>
         <div className="content__description">
           <p>
-            <strong>Population: </strong>81,770,900
+            <strong>Population: </strong>{props.population.toLocaleString()}
           </p>
           <p>
-            <strong>Region: </strong>Europe
+            <strong>Region: </strong>{props.region}
           </p>
           <p>
-            <strong>Capital: </strong>Berlin
+            <strong>Capital: </strong>{props.capital || '-'}
           </p>
         </div>
       </div>
