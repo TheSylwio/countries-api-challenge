9cb9eae196ffab8d3e3e07ad1eb2ee0eea1fa111
thesylwio <thesylwio@gmail.com>
2020-10-22 20:44:21 +0200

    🔧✨ Refactored Navbar component

---
+++ src/components/Navbar/Navbar.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const StyledNavbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8pt;
+  box-shadow: 0 1px 15px -5px hsl(0, 0%, 70%);
+`;
+
+export const NavbarHeader = styled.h1`
+  font-weight: 800;
+`;
+
+export const DarkModeButton = styled.button`
+  background: #FAFAFA;
+  height: 3em;
+  font-weight: 300;
+  border-color: transparent;
+  outline: 0;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+// TODO: DELETE ME
+// .navbar__button span {
+//   margin-left: 8pt;
+// }
