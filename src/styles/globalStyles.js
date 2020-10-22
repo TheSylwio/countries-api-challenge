14e49ec3c31850091b22561733ef0808077bd49a
thesylwio <thesylwio@gmail.com>
2020-10-22 20:09:09 +0200

    ✨ Created Global Styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  
+  html, body {
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 14px;
+    padding: 0;
+    margin: 0;
+    background: hsl(0, 0%, 98%);
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default globalStyles;
