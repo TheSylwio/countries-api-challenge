cb36b8c3b534aa34738358dbe4190e76a5072d55
thesylwio <thesylwio@gmail.com>
2020-10-23 16:05:55 +0200

    ✨👐 App now uses store

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import Homepage from './pages/Homepage';
 import Country from './pages/Country/Country';
 import theme from './styles/theme';
 import GlobalStyles from './styles/globalStyles';
+import initializeStore from './redux/store';
 
 const App = () => {
+  const store = initializeStore();
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Route path='/' component={Homepage} exact />
-          <Route path='/:country' component={Country} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Router>
+          <Switch>
+            <Route path='/' component={Homepage} exact />
+            <Route path='/:country' component={Country} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
