df4c01853096f65ca08101f6bdd8e30d16ef0964
thesylwio <thesylwio@gmail.com>
2020-10-23 13:28:51 +0200

    🧹 Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Country from "./pages/Country/Country";
-import {ThemeProvider} from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import Homepage from './pages/Homepage';
+import Country from './pages/Country/Country';
 import theme from './styles/theme';
-import GlobalStyles from './styles/globalStyles'
+import GlobalStyles from './styles/globalStyles';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Router>
         <Switch>
-          <Route path='/' component={Homepage} exact/>
-          <Route path='/:country' component={Country}/>
+          <Route path='/' component={Homepage} exact />
+          <Route path='/:country' component={Country} />
         </Switch>
       </Router>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
